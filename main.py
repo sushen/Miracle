1a1881cf17ffce2333357f2111d145740a3ae4f6
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-05-15 12:26:56 +0600

    focebook post work fine and entertain with flower

--- main.py
+++ main.py
@@ -7,60 +7,98 @@
 #   !/usr/bin/env python
 #   coding: utf-8
 
-from selenium import webdriver
-import time
-import random
 import os
+import time
+from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 
-chrome_options = Options()
-chrome_options.add_argument("--start-maximized")
-driver = webdriver.Chrome("./chromedriver.exe", chrome_options=chrome_options)
-chrome_options.add_argument("user-data-dir=chrome-data")
-chrome_options.add_argument('--profile-directory=Default')
-# prefs = {"profile.default_content_setting_values.notifications" : 2}
-# chrome_options.add_experimental_option("prefs",prefs)
-chrome_options.add_argument('disable-infobars')
-chrome_options.add_experimental_option("useAutomationExtension", False)
-chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
-actions = ActionChains(driver)
+def driver():
+    global driver
+    driver = webdriver.Chrome("./chromedriver.exe", chrome_options=chrome_options)
+    # driver.get("https://facebook.com")
 
 
+def chrome_options():
+    global chrome_options
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--profile-directory=Default')
+    # chrome_options.add_argument("--user-data-dir=chrome-data")
+    prefs = {"profile.default_content_setting_values.notifications" : 2}
+    chrome_options.add_experimental_option("prefs",prefs)
+    chrome_options.add_argument('disable-infobars')
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+
+def actions():
+    global actions
+    actions = ActionChains(driver)
+
+
+def login():
+    try:
+        # I use environment veriable  base on this tutorials https://www.youtube.com/watch?v=IolxqkL7cD8
+        username = os.environ.get('my_facebook_username')
+        password = os.environ.get('my_facebook_password')
+
+        driver.find_element_by_name("email").send_keys(username)
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_name("login").click()
+        print(input("Press any Key: "))
+        print("Login work Successfully ")
+
+    except:
+        pass
+
+
+def navigatePagePostAria():
+    sleepTime = 4
+    implicitlyWaitTime = 20
+    for i in range(2):
+        driver.implicitly_wait(implicitlyWaitTime)
+        actions.send_keys(Keys.BACK_SPACE)
+        actions.send_keys(Keys.TAB * 10)
+        time.sleep(sleepTime)
+        actions.perform()
+        print("Firast 10 tabs Working")
+
+    actions.send_keys(Keys.TAB * 6)
+    actions.send_keys(Keys.ENTER)
+    actions.perform()
+    print("Navigate Post area Successfully ")
+
+
+def activePostAreaAndPostInPage():
+    sleepTime = 4
+    implicitlyWaitTime = 20
+    time.sleep(sleepTime)
+
+    active_post_area = driver.switch_to.active_element
+    driver.implicitly_wait(implicitlyWaitTime)
+    time.sleep(sleepTime)
+    active_post_area.send_keys("'driver.switch_to.active_element' "
+                               "this code is a one of important snippet for facebook automation.")
+
+    actions.perform()
+    print("Writing Post in the post area Successfully ")
+
+    for i in range(2):
+        driver.implicitly_wait(implicitlyWaitTime)
+        actions.send_keys(Keys.TAB * 5)
+        print(str(i) + " tabs Working")
+    actions.send_keys(Keys.ENTER)
+    actions.perform()
+
+
+chrome_options()
+driver()
 driver.get("https://facebook.com")
-
-try:
-    # I use environment veriable  base on this tutorials https://www.youtube.com/watch?v=IolxqkL7cD8
-    username = os.environ.get('my_facebook_username')
-    password = os.environ.get('my_facebook_password')
-
-    driver.find_element_by_name("email").send_keys(username)
-    driver.find_element_by_name("pass").send_keys(password)
-    driver.find_element_by_name("login").click()
-
-except:
-    pass
-print(input("Press any Key: "))
-
+actions()
+login()
 driver.get("https://www.facebook.com/sushen.biswas/")
-print(input("Press any Key: "))
-driver.implicitly_wait(5)
-actions.send_keys(Keys.BACK_SPACE)
-actions.send_keys(Keys.TAB * 56)
-actions.send_keys(Keys.ENTER)
-actions.perform()
-
-print(input("Press any Key: "))
-driver.implicitly_wait(10)
-active_post_area = driver.switch_to.active_element
-active_post_area.send_keys("'driver.switch_to.active_element' "
-                           "this code is a one of important snippet for facebook automation.")
-actions.send_keys(Keys.TAB * 3)
-actions.send_keys(Keys.BACK_SPACE)
-driver.implicitly_wait(10)
-actions.send_keys(Keys.BACK_SPACE)
-actions.send_keys(Keys.TAB * 3)
-actions.send_keys(Keys.ENTER)
-actions.perform()
+navigatePagePostAria()
+activePostAreaAndPostInPage()
